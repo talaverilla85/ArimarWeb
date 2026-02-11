93b77e5da3df93433ac7e02b5c4496aeed682645
Noe Gómez <proyectoencanarias@gmail.com>
2026-02-11 01:06:34 +0000

    mapa

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -18,7 +18,7 @@ export const siteConfig = {
   },
   /** URL de Google Maps para la ubicación */
   mapsUrl:
-    'https://www.google.com/maps/search/?api=1&query=Avenida+Poliz%C3%B3n+67,+Playa+de+Arinaga,+Ag%C3%BCimes',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14109.768197235702!2d-15.414344072341878!3d27.85768820651033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc40a1a45ac285f7%3A0xf8a78393826fee5!2sAriMar!5e0!3m2!1ses!2ses!4v1770771808126!5m2!1ses!2ses',
 } as const
 
 /**
